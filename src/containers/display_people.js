d6ad859b54104a2df972fde84684591f222921b9
AyodeleOluwabusola <oluwabusola.ayodele.com>
2018-07-13 09:58:56 +0100

    new display_people

--- src/containers/display_people.js
+++ src/containers/display_people.js
@@ -8,68 +8,48 @@ class DisplayPeople extends Component{
     // }
 
     renderPeopleData(peopleData){
+           
         //console.log(peopleData.persons.length)
-        for(let i=0; i<=peopleData.persons.length; i++){
-            let name=[];
-            const name1 = peopleData.persons.map(person=> person.name);
-            const age1 = peopleData.persons.map(person => person.age);
-
-            //const age = peopleData.persons[i].age;
-            //const name1 = peopleData.persons[i].name;
-            //name.push(name1)
-            // console.log(name1)
+        for (let i = 0; i < peopleData.persons.length; i++){
+            var displayPeople = peopleData.persons
         
-        return(
-         <tr key ='persons'>
-                <td>{name1[0]}</td>
-                <td>{age1[0]}</td>           
-         </tr>
-            <tr key='persons'>
-                <td>{name1[1]}</td>
-                <td>{age1[1]}</td>
-            </tr>
-            <tr key='persons'>
-                <td>{name1[2]}</td>
-                <td>{age1[2]}</td>
-            </tr>
-            <tr key='persons'>
-                
-                <td>{name1[3]}</td>
-                <td>{age1[3]}</td>
-            </tr>
-            <tr key='persons'>
-                <td>{name1[4]}</td>
-                <td>{age1[4]}</td>
-            </tr>
-            <tr key='persons'>
-                <td>{name1[5]}</td>
-                <td>{age1[0]}</td>
-            </tr>
-            <tr key='persons'>
-                <td>{name1[6]}</td>
-                <td>{age1[0]}</td>
-            </tr>
-            
-        )     
+        
+         return(
+                <tr key={displayPeople[i].name[i]}>
+                    <td>{displayPeople[i].name}</td>
+                 <td><img src={displayPeople[i].photo_thumb} alt={displayPeople[i].name}/></td>
+                    <td>{displayPeople[i].age}</td>
+                </tr>
+                // <tr key={name1[1]}>
+                //     <td>{name1[1]}</td>
+                //     <td>{age1[1]}</td>
+                // </tr>
+      
+            );
+        
+        }
     }
-}
+
+
 
      render(){
-         return(
+        return(
              <table className="table table-hover">
                 <thead>
                     <tr>
-                        <td>Name</td>
-                        <td>Age</td>      
+                        <th>Name</th>
+                        <th>Image</th> 
+                        <th>Age</th>      
                     </tr>
                 </thead>
                 <tbody>
                     {this.props.people.map(this.renderPeopleData)}
                 </tbody>
              </table>
-         )
+        );
     }
 }
+
 
 function mapStateToProps(state) {
     return {
